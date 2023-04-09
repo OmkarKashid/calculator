019a0f4bcab1a35481c684da50447f95de724e5c
OmkarKashid <68742874+OmkarKashid@users.noreply.github.com>
2023-04-10 00:18:05 +0530

    add equal btn functions

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 function add(num1 , num2){
-    return num1 + num2;
+    return (+num1) + (+num2);
 }
 function subtract(num1 , num2){
     return num1 - num2;
@@ -10,7 +10,7 @@ function multiply(num1 , num2){
 function divide(num1 , num2){
     return num1 / num2;
 }
-function operate(num1, num2, operator){
+function operate(num1, operator, num2){
     switch(operator){
         case "+":
             return add(num1, num2);
@@ -28,7 +28,12 @@ function refreshDisplay(){
     disp.textContent = displayValue;
 }
 function display(btn){
-    displayValue = displayValue.concat(` ${btn.textContent}`);
+    if(operators.includes(btn.textContent)){
+        displayValue = displayValue.concat(` ${btn.textContent} `);
+    }
+    else{
+    displayValue = displayValue.concat(`${btn.textContent}`);
+    }
     refreshDisplay();
     console.log(displayValue);
 }
@@ -42,17 +47,26 @@ function iterateEveryButton(){
 function addButtonEvent(btn){
     btn.addEventListener("click", () => display(btn))
 }
+function processEqual(){
+    const equationArray = displayValue.split(" ");
+    console.log(equationArray[1]);
+    displayValue = String(operate(equationArray[0], equationArray[1], equationArray[2]));
+    refreshDisplay();
+}
 function clearDisplay(){
     displayValue = "";
     refreshDisplay();
 }
 const disp = document.querySelector("#display");
+const eql = document.querySelector("#btn_eql");
 const clr = document.querySelector("#btn_clr");
+const operators = ["+", "-", "*", "/"];
 let displayValue = "";
 let num1 = 0;
 let num2 = 0;
 let operator = "";
 iterateEveryButton();
+eql.addEventListener("click", () => processEqual());
 clr.addEventListener("click", () => clearDisplay());
 
 
